a077a6d92acce5638dbfb0e9695e274d112ec5b6
Pramod144 <pramody1444@gmail.com>
2023-04-27 16:52:38 +0530

    save the change

--- demo.js
+++ demo.js
@@ -1,7 +1,16 @@
-let headerTitle=document.getElementById('header-title');
+// let headerTitle=document.getElementById('header-title');
 
- headerTitle.textContent='hello';
- headerTitle.innerText='good bye';
- //headerTitle.style.border='solid #000';
- let newHeader=document.getElementById('main-header');
- newHeader.style.borderBottom='solod #000';
+//  headerTitle.textContent='hello';
+//  headerTitle.innerText='good bye';
+//  //headerTitle.style.border='solid #000';
+//  let newHeader=document.getElementById('main-header');
+//  newHeader.style.borderBottom='solod #000';
+let items=document.getElementsByClassName('list-group-item');
+items[2].style.background='green';
+ items[0].style.fontWeight='bold';
+ items[1].style.fontWeight='bold';
+ items[2].style.fontWeight='bold';
+ items[3].style.fontWeight='bold';
+
+ let grpitems=document.getElementsById('items');
+ grpitems.style.fontWeight='bold';
