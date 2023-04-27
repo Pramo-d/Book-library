13157ad409e5cbc5818e01b58337a86466fa125b
Pramod144 <pramody1444@gmail.com>
2023-04-27 12:49:16 +0530

    final commit

---
+++ demo.js
@@ -0,0 +1,7 @@
+let headerTitle=document.getElementById('header-title');
+
+ headerTitle.textContent='hello';
+ headerTitle.innerText='good bye';
+ //headerTitle.style.border='solid #000';
+ let newHeader=document.getElementById('main-header');
+ newHeader.style.borderBottom='solod #000';
