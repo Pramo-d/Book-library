25864679daf1be0be5b9f6cc785a816ad1e9a436
Pramod144 <pramody1444@gmail.com>
2023-04-28 08:18:30 +0530

    modified  version
    modified  version

--- demo.js
+++ demo.js
@@ -6,7 +6,11 @@
 //  let newHeader=document.getElementById('main-header');
 //  newHeader.style.borderBottom='solod #000';
 let items=document.getElementsByClassName('list-group-item');
-items[2].style.background='green';
+//items[2].style.background='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+    items[i].style.backgroundColor='grey'; 
+}
  items[0].style.fontWeight='bold';
  items[1].style.fontWeight='bold';
  items[2].style.fontWeight='bold';
@@ -14,3 +18,9 @@ items[2].style.background='green';
 
  let grpitems=document.getElementsById('items');
  grpitems.style.fontWeight='bold';
+
+ //getelementbytag
+  let value=document.getElementsByTagName('li');
+ for(let i=0;i<value.length;i++){
+value[i].style.backgroundColor='grey';
+ }
