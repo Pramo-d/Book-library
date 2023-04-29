dda5ea86cecfe55b33639ed198f7f690bfbab3b6
Pramod144 <pramody1444@gmail.com>
2023-04-29 14:18:14 +0530

    local storage value set & get

--- local storage/local.js
+++ local storage/local.js
@@ -2,3 +2,13 @@ let val=document.getElementById('main');
 val.style.backgroundColor='grey';
  
 localStorage.setItem('name', 'rohan');
+
+  let obj={
+  name:'Ram',
+  age:'26'
+  
+  }
+  let obj1=JSON.stringify(obj);
+  localStorage.setItem('newObj',obj1);
+    
+let objs=JSON.parse(localStorage.getItem('obj'));
